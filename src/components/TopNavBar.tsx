0138912929f5a37a084026a61106d580bd39feb0
lesliexin <leslie.xin@hotmail.com>
2020-05-10 10:02:05 -0400

    small changes

--- src/components/TopNavBar.tsx
+++ src/components/TopNavBar.tsx
@@ -1,25 +1,34 @@
-import React from 'react';
+import React from "react";
 import { useHistory } from "react-router-dom";
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const Wrapper = styled.div`
-    display: flex;
-  background-color: #F7F9FF;
+  display: flex;
+  flex-direction: row;
+  background-color: #f7f9ff;
+  justify-content: flex-end;
+`;
+
+const NavOption = styled.div`
+  padding: 32px;
 `;
 
 export const TopNavBar = () => {
+  const history = useHistory();
 
-    const history = useHistory();
-
-    const routeChange = () =>{ 
-        let path = `newPath`; 
-        history.push(path);
-      }
-    return (<Wrapper >
-        <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">about</Link></li>
-        </ul>
-    </Wrapper>);
-  }
+  const routeChange = () => {
+    let path = `newPath`;
+    history.push(path);
+  };
+  return (
+    <Wrapper>
+      <NavOption>
+        <Link to="/">Home</Link>
+      </NavOption>
+      <NavOption>
+        <Link to="/about">about</Link>
+      </NavOption>
+    </Wrapper>
+  );
+};
